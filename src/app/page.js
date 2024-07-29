e527c60c6b20ced461abd47c76d2c54684c103cf
Cwolfberry <ysbzc1009@gmail.com>
2024-07-29 10:02:25 +0800

    body init.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,24 @@
 import React from "react";
 import HomeHeader from "../components/HomeHeader";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
+      {/** header */}
       <HomeHeader />
+
+      {/** body */}
+      {/* <div className="">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png"
+          alt="Google Logo"
+          width={300}
+          height={100}
+          priority
+          style={{ width: "auto" }}
+        />
+      </div> */}
     </>
   );
 }
